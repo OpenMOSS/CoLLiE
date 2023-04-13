09428cf9ba8ae5554ff383681cdb087bf855bb18
StarLooo <2056940367@qq.com>
2023-04-13 17:32:31 +0800

    1. use adam for better generation 2. rollback to old version of generate because the new one has some unfixed fatal bug

--- tunelite/trainer/colossalai_trainer.py
+++ tunelite/trainer/colossalai_trainer.py
@@ -1,5 +1,4 @@
 import sys
-
 sys.path.append("..")
 
 from .utils import GPTLMLoss, sample_top_p
@@ -22,7 +21,6 @@ except ModuleNotFoundError:
     raise ModuleNotFoundError(
         "Detected Colossal-AI is not installed. See https://github.com/hpcaitech/ColossalAI")
 
-
 @dataclass
 class TrainerArgs:
     learning_rate: float = field(
@@ -31,22 +29,22 @@ class TrainerArgs:
     epochs: int = field(
         default=10,
         metadata={"help": "Total number of training epochs to perform. "
-                          "If it is set to -1, the training will run forever."
-                          "If it is set to 0, the training will not run."})
+                  "If it is set to -1, the training will run forever."
+                  "If it is set to 0, the training will not run."})
     eval_per_steps: int = field(
         default=10,
-        metadata={"help": "The number of steps to perform evaluation. "
-                          "If it is set to -1, the evaluation will run after every training step."
-                          "If it is set to 0, the evaluation will not run after training step."})
+        metadata={"help": "The number of steps to perform evaluation. " 
+                  "If it is set to -1, the evaluation will run after every training step."
+                  "If it is set to 0, the evaluation will not run after training step."})
     eval_per_epoches: int = field(
         default=1,
         metadata={"help": "The number of epochs to perform evaluation. "
-                          "If it is set to -1, the evaluation will run after every training epoch."
-                          "If it is set to 0, the evaluation will not run after training epoch."})
+                  "If it is set to -1, the evaluation will run after every training epoch."
+                  "If it is set to 0, the evaluation will not run after training epoch."})
     eval_max_length: int = field(
         default=128,
         metadata={"help": "The maximum length of generated text when evaluating."
-                          "If it is set to -1, the evaluation will run until the stop tokens are generated."})
+                  "If it is set to -1, the evaluation will run until the stop tokens are generated."})
     eval_stop_tokens: List[int] = field(
         default_factory=partial(list, [2]),
         metadata={"help": "The stop tokens when evaluating or generating."})
@@ -56,13 +54,12 @@ class TrainerArgs:
     eval_temperature: float = field(
         default=1.0,
         metadata={"help": "The temperature when evaluating or generating."})
-    eval_use_cache: bool = field(
+    eval_use_cache : bool = field(
         default=True,
         metadata={"help": "Whether to use key/value cache when evaluating or generating."})
-    inplace: bool = field(
+    inplace : bool = field(
         default=False,
         metadata={"help": "Whether to use inplace gradient update."})
-
 
 def inplace_grad(model, lr=5e-4, micro_batch_num: int = 1):
     def func(x):
@@ -72,9 +69,7 @@ def inplace_grad(model, lr=5e-4, micro_batch_num: int = 1):
                     p.data -= (lr * p.grad.data) / micro_batch_num
                     p.grad = None
         return x
-
     return func
-
 
 class ColossalaiTrainer:
     def __init__(self,
@@ -82,19 +77,18 @@ class ColossalaiTrainer:
                  tokenizer,
                  train_dataloader,
                  eval_dataloader,
-                 compute_metrics=None,
+                 compute_metrics = None,
                  trainer_args: TrainerArgs = TrainerArgs()) -> None:
         self.model = model
         self.tokenizer = tokenizer
         self.compute_metrics = compute_metrics
         self.trainer_args = trainer_args
         if self.trainer_args.inplace:
-            self.grad_func = inplace_grad(model, lr=trainer_args.learning_rate,
-                                          micro_batch_num=model.model_args.micro_batch_num)
+            self.grad_func = inplace_grad(model, lr=trainer_args.learning_rate, micro_batch_num=model.model_args.micro_batch_num)
             for n, p in self.model.named_parameters():
                 p.register_hook(self.grad_func)
-        self.optimizer = torch.optim.SGD(self.model.parameters(), lr=trainer_args.learning_rate)
-        # self.optimizer = torch.optim.Adam(self.model.parameters(), lr=trainer_args.learning_rate)
+        # self.optimizer = torch.optim.SGD(self.model.parameters(), lr=trainer_args.learning_rate)
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=trainer_args.learning_rate)
         self.engine, self.train_dataloader, self.eval_dataloader, _ = colossalai.initialize(
             model=self.model,
             train_dataloader=train_dataloader,
@@ -105,7 +99,6 @@ class ColossalaiTrainer:
 
     def train(self):
         self.engine.train()
-
         def train_loop(epoch: int = 0):
             with tqdm.tqdm(self.train_dataloader, disable=not gpc.is_pipeline_last_stage()) as tqb:
                 for step, batch in enumerate(tqb, start=1):
@@ -128,7 +121,6 @@ class ColossalaiTrainer:
                         self.eval(epoch, step)
                     elif self.trainer_args.eval_per_steps > 0 and step % self.trainer_args.eval_per_steps == 0:
                         self.eval(epoch, step)
-
         if self.trainer_args.epochs == 0:
             return
         elif self.trainer_args.epochs == -1:
@@ -159,6 +151,64 @@ class ColossalaiTrainer:
                  use_cache: bool = True,
                  stop_tokens: List[int] = [2]):
         assert input_ids.ndim == 2, "input_ids must be 2D tensor (B, N)"
+        # min_length = input_ids.argmin(dim=1)
+        # min_length = min(min_length.masked_fill(min_length == 0, 1024))
+        min_length = min([len(torch.nonzero(sample)) for sample in input_ids])
+        assert min_length >= 1
+        if max_length > input_ids.shape[1]:
+            input_ids = torch.nn.functional.pad(input_ids, (0, max_length - input_ids.shape[1]), value=0)
+        assert input_ids.shape[1] >= max_length
+        # generate loop:
+        with tqdm.tqdm(range(min_length, max_length), disable=not gpc.is_pipeline_last_stage()) as tqb:
+            # flag vector to control early stop batch generate
+            stop_generate_flag_vector = [False for _ in range(len(input_ids))]
+            last_pos = 0
+            for current_pos in tqb:
+                try:
+                    self.engine.eval()
+                    hidden_states, label, _ = self.engine.execute_schedule(
+                        cycle([({
+                                    # "input_ids": input_ids[:, last_pos:current_pos],
+                                    "input_ids": input_ids[:, :current_pos], # if you don't use key/value cache
+                                    # "use_cache": torch.ones(input_ids.shape[0], dtype=torch.bool)
+                                    "use_cache": torch.zeros(input_ids.shape[0], dtype=torch.bool) # if you don't use key/value cache
+                                }, input_ids)]),
+                        forward_only=True,
+                        return_loss=False,
+                        return_output_label=True,
+                    )
+                    last_pos = current_pos
+                    next_tokens = torch.zeros(input_ids.shape[0], 1, dtype=torch.long).to(
+                        torch.device(f"cuda:{os.environ['LOCAL_RANK']}"))
+                    if gpc.is_pipeline_last_stage():
+                        next_tokens = torch.argmax(hidden_states[:, -1, :], dim=-1)
+                        next_tokens = torch.unsqueeze(next_tokens, dim=-1)
+                    torch.distributed.broadcast(next_tokens, src=gpc.get_world_size(ParallelMode.PIPELINE) - 1)
+                    next_tokens = next_tokens.to(input_ids.device)
+                    input_ids[:, current_pos] = torch.where(input_ids[:, current_pos] == 0, next_tokens[:, 0],
+                                                            input_ids[:, current_pos])
+                    tqb.set_postfix({'generating': f"{current_pos}/{max_length}"})
+                    # check if can stop generate using stop_generate_flag_vector
+                    for stop_token in stop_tokens:
+                        for i in range(len(stop_generate_flag_vector)):
+                            if not stop_generate_flag_vector[i] and \
+                                    stop_token in input_ids[i][:current_pos]:
+                                stop_generate_flag_vector[i] = True
+                    if all(stop_generate_flag_vector):
+                        raise StopIteration
+                    torch.cuda.empty_cache()
+                except StopIteration:
+                    break
+        return input_ids
+
+
+    @torch.no_grad()
+    def _generate(self,
+                 input_ids: torch.Tensor,
+                 max_length: int = 1024,
+                 use_cache: bool = True,
+                 stop_tokens: List[int] = [2]):
+        assert input_ids.ndim == 2, "input_ids must be 2D tensor (B, N)"
         min_len = min([len(torch.nonzero(sample)) for sample in input_ids])
         if max_length > input_ids.shape[1]:
             input_ids = torch.nn.functional.pad(input_ids, (0, max_length - input_ids.shape[1]), value=0)
@@ -173,32 +223,25 @@ class ColossalaiTrainer:
                     self.engine.eval()
                     hidden_states, label, _ = self.engine.execute_schedule(
                         cycle([({
-                                    "input_ids": input_ids[working_batch_idx,
-                                                 cached_len:current_pos] if use_cache else input_ids[working_batch_idx,
-                                                                                           :current_pos],
-                                    "use_cache": torch.ones(input_ids.shape[0],
-                                                            dtype=torch.bool) if use_cache else torch.zeros(
-                                        input_ids.shape[0], dtype=torch.bool)
-                                }, input_ids)]),
+                            "input_ids": input_ids[working_batch_idx, cached_len:current_pos] if use_cache else input_ids[working_batch_idx, :current_pos],
+                            "use_cache": torch.ones(input_ids.shape[0], dtype=torch.bool) if use_cache else torch.zeros(input_ids.shape[0], dtype=torch.bool)
+                        }, input_ids)]),
                         forward_only=True,
                         return_loss=False,
                         return_output_label=True,
                     )
                     cached_len = current_pos
-                    next_tokens = torch.zeros(input_ids.shape[0], 1, dtype=torch.long).to(
-                        torch.device(f"cuda:{os.environ['LOCAL_RANK']}"))
+                    next_tokens = torch.zeros(input_ids.shape[0], 1, dtype=torch.long).to(torch.device(f"cuda:{os.environ['LOCAL_RANK']}"))
                     if gpc.is_pipeline_last_stage():
                         next_tokens = torch.argmax(hidden_states[:, -1, :], dim=-1)
                         next_tokens = torch.unsqueeze(next_tokens, dim=-1)
                     torch.distributed.broadcast(next_tokens, src=gpc.get_world_size(ParallelMode.PIPELINE) - 1)
                     next_tokens = next_tokens.to(input_ids.device)
-                    input_ids[working_batch_idx, current_pos] = torch.where(
-                        input_ids[working_batch_idx, current_pos] == 0, next_tokens[:, 0],
-                        input_ids[working_batch_idx, current_pos])
+                    input_ids[working_batch_idx, current_pos] = torch.where(input_ids[working_batch_idx, current_pos] == 0, next_tokens[:, 0], input_ids[working_batch_idx, current_pos])
                     tqb.set_postfix({'generating': f"{current_pos}/{max_length}"})
                     for i in range(len(torch.flatten(input_ids[:, current_pos]).tolist())):
-                        if torch.flatten(input_ids[:, current_pos]).tolist()[i] in stop_tokens:
-                            stop_flag[i] = True
+                            if torch.flatten(input_ids[:, current_pos]).tolist()[i] in stop_tokens:
+                                stop_flag[i] = True
                     torch.cuda.empty_cache()
                 except StopIteration:
                     break
