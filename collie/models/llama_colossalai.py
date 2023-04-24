237c1d884dca0f26cc9fe307a7fc1a67b15aa07e
zhangshuo <zhangshuolove@live.com>
2023-04-24 19:55:49 +0800

    fix some bugs

--- collie/models/llama_colossalai.py
+++ collie/models/llama_colossalai.py
@@ -534,9 +534,6 @@ class Transformer(nn.Module):
                 input_ids: Optional[torch.Tensor] = None,
                 use_cache: torch.Tensor = torch.zeros(1, dtype=torch.bool),
                 **kwargs):
-        if gpc.is_pipeline_first_stage():
-            import pdb
-            pdb.set_trace()
         if self.is_start:
             assert input_ids is not None, "`input_ids` is not allowed to be None in the first pipeline node. "
             hidden_states = self.token_embedding(input_ids)
