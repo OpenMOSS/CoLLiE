25a765b7fc60d2c397b2ac99f9b7a6801e25f7ca
x54-729 <17307130121@fudan.edu.cn>
2023-05-09 11:11:23 +0800

    add from_pretrained to Arguments

--- collie/trainer/arguments.py
+++ collie/trainer/arguments.py
@@ -1,6 +1,10 @@
 from dataclasses import dataclass, field
-
 from typing import Union
+
+from deepspeed.runtime.config import DeepSpeedConfig
+import sys
+sys.path.append("../../")
+from collie.log import logger
 
 @dataclass
 class Arguments:
@@ -60,6 +64,37 @@ class Arguments:
             "help": "DeepSpeed configuration file."
         }
     )
+
+    @classmethod
+    def from_pretrained(cls, path: str, **kwargs):
+        json_config = load_config(path)
+        unexpected = set()
+        init_dict = {}
+        for key, value in json_config.items():
+            if key in cls.__dict__:
+                init_dict[key] = value
+            else:
+                unexpected.add(key)
+        for key, value in kwargs.items():
+            if key in cls.__dict__:
+                init_dict[key] = value
+            else:
+                unexpected.add(key)
+
+        if len(unexpected) != 0:
+            logger.rank_zero_warning(
+                f"The following arguments from `from_pretrained` are not "
+                f"defined in {cls.__class__.__name__} and will be ignored:\n"
+                f"{list(unexpected)}"
+            )
+
+        return cls(**init_dict)
+    
+    def __post_init__(self):
+        if isinstance(self.ds_config, str):
+            self.ds_config = load_config(self.ds_config)
+        assert isinstance(self.ds_config, dict), self.ds_config
+
     
 def load_config(path: str):
     content = {}
@@ -70,4 +105,3 @@ def load_config(path: str):
         import json
         content = json.load(open(path, "r"))
     return content
-        
