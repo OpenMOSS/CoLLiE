2f17b8bf48c3f8005566ec95692d0f2a7ac22328
Carol-gutianle <2311601671@qq.com>
2023-05-16 19:14:28 +0800

    example/alapaca

--- examples/alpaca/alpaca.py
+++ examples/alpaca/alpaca.py
@@ -1,5 +1,5 @@
 # The simplist example of using collie, enjoy 3D parallism!
-# Command CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 torchrun --rdzv_backend=c10d --rdzv_endpoint=localhost:29402 --nnodes=1 --nproc_per_node=8 simple.py
+# Command CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 torchrun --rdzv_backend=c10d --rdzv_endpoint=localhost:29402 --nnodes=1 --nproc_per_node=8 alpaca.py
 import sys
 sys.path.append("/mnt/petrelfs/gutianle/Megatron-LM/")
 sys.path.append("/mnt/petrelfs/gutianle/collie/")
@@ -12,16 +12,6 @@ from transformers.generation.utils import GenerationConfig
 from torch.utils.data import Dataset
 import torch
 import json
-
-args = LlamaArguments(
-    use_flash=True,checkpointing=False,seed=42,pp_size=2,tp_size=2,dp_size=2,dropout=0,
-    ds_config={
-        "train_micro_batch_size_per_gpu": 1,
-        "train_batch_size": 1,
-        "gradient_accumulation_steps": 1,
-        "fp16": {"enabled": True},
-    }
-)
 
 tokenizer = LlamaTokenizer.from_pretrained("decapoda-research/llama-7b-hf", 
                                            padding_side="left", 
@@ -62,7 +52,7 @@ class AlpacaDataset(Dataset):
     def __getitem__(self, idx):
         return self.alpaca[idx]
     
-def collate_fn(batch, tokenizer):
+def collate_fn(batch):
     batch_inputs = []
     batch_labels = []
     for sample in batch:
@@ -70,18 +60,15 @@ def collate_fn(batch, tokenizer):
         sample_label = sample['prompt'] + sample['label']
         batch_inputs.append(sample_prompt)
         batch_labels.append(sample_label)
-    input_ids = tokenizer(batch_inputs, return_tensors='pt', padding=True)
-    label_ids = tokenizer(batch_labels, return_tensors='pt', padding=True)
+    input_ids = tokenizer(batch_inputs, return_tensors='pt')['input_ids']
+    label_ids = tokenizer(batch_labels, return_tensors='pt')['input_ids']
     return input_ids, label_ids
 
-    
-torch.set_default_tensor_type(torch.HalfTensor)
 dataset = AlpacaDataset('alpaca_data.json')
 train_dataset = dataset[:-32]
 eval_dataset = dataset[-32:]
 
 model = LlamaModel(args)
-optimizer = torch.optim.SGD(model.parameters(), lr=1e-6)
 state_dict = LlamaModel.load_parallel_state_dict(
     path="hdd:s3://opennlplab_hdd/models/llama/llama-7b-hf/",
     protocol="petrel",
@@ -89,6 +76,7 @@ state_dict = LlamaModel.load_parallel_state_dict(
     process_exclusion=False,
     args=args)
 model.load_state_dict(state_dict)
+
 trainer = Trainer(
     model=model,
     train_dataset=train_dataset,
@@ -100,4 +88,4 @@ trainer = Trainer(
     args=args
 )
 torch.cuda.empty_cache()
-trainer.eval()
+trainer.train()
