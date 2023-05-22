b8d167b0a9d34d9bfb86fe9937bc40cb25a05c5b
MorningForest <2297662686@qq.com>
2023-05-22 19:02:08 +0800

    增加acc metric

---
+++ collie/metrics/accuracy.py
@@ -0,0 +1,108 @@
+from typing import Any, Dict, Optional
+
+import torch
+import numpy as np
+
+from collie.metrics.base import BaseMetric
+
+
+def seq_len_to_mask(seq_len, max_len: Optional[int]=None):
+    r"""
+    将一个表示 ``sequence length`` 的一维数组转换为二维的 ``mask`` ，不包含的位置为 **0**。
+    .. code-block::
+        >>> seq_len = torch.arange(2, 16)
+        >>> mask = seq_len_to_mask(seq_len)
+        >>> print(mask.size())
+        torch.Size([14, 15])
+        >>> seq_len = np.arange(2, 16)
+        >>> mask = seq_len_to_mask(seq_len)
+        >>> print(mask.shape)
+        (14, 15)
+        >>> seq_len = torch.arange(2, 16)
+        >>> mask = seq_len_to_mask(seq_len, max_len=100)
+        >>>print(mask.size())
+        torch.Size([14, 100])
+    :param seq_len: 大小为 ``(B,)`` 的长度序列；
+    :param int max_len: 将长度补齐或截断到 ``max_len``。默认情况（为 ``None``）使用的是 ``seq_len`` 中最长的长度；
+        但在 :class:`torch.nn.DataParallel` 等分布式的场景下可能不同卡的 ``seq_len`` 会有区别，所以需要传入
+        ``max_len`` 使得 ``mask`` 的补齐或截断到该长度。
+    :return: 大小为 ``(B, max_len)`` 的 ``mask``， 元素类型为 ``bool`` 或 ``uint8``
+    """
+    max_len = int(max_len) if max_len is not None else int(seq_len.max())
+
+    assert seq_len.ndim == 1, f"seq_len can only have one dimension, got {seq_len.ndim == 1}."
+    batch_size = seq_len.shape[0]
+    broad_cast_seq_len = torch.arange(max_len).expand(batch_size, -1).to(seq_len)
+    mask = broad_cast_seq_len < seq_len.unsqueeze(1)
+    return mask
+
+    
+class Accuracy(BaseMetric):
+
+    def __init__(self, gather_result: bool=False):
+        super().__init__(gather_result=gather_result)
+        self.correct = 0
+        self.total = 0
+    
+    def get_metric(self, reset:bool=True)->Dict:
+        r"""
+        :meth:`get_metric` 函数将根据 :meth:`update` 函数累计的评价指标统计量来计算最终的评价结果。
+
+        :return dict evaluate_result: {"acc": float, 'total': float, 'correct': float}
+        """
+        evaluate_result = {'acc': round(self.correct / (self.total + 1e-12), 6),
+                           'total': self.total, 'correct': self.correct}
+        # reset 
+        if reset:
+            self.total = 0
+            self.correct = 0 
+        return evaluate_result
+    
+    def update(self, result:Dict):
+        r"""
+        :meth:`update` 函数将针对一个批次的预测结果做评价指标的累计。
+        :param result 类型为Dict且keys至少包含["pred", "target"]
+            pred: 预测的 tensor, tensor 的形状可以是 ``torch.Size([B,])`` 、``torch.Size([B, n_classes])`` 、
+            ``torch.Size([B, max_len])`` 或 ``torch.Size([B, max_len, n_classes])``
+            target: 真实值的 tensor, tensor 的形状可以是 ``torch.Size([B,])`` 、``torch.Size([B, max_len])``
+            或 ``torch.Size([B, max_len])``
+            seq_len: 序列长度标记, 标记的形状可以是 ``None``,  或者 ``torch.Size([B])`` 。
+            如果 mask 也被传进来的话 ``seq_len`` 会被忽略
+        """
+        if "pred" not in result.keys():
+            raise ValueError(f"pred not in result!")
+        if "target" not in result.keys():
+            raise ValueError(f"target not in result!")
+        pred = result.get("pred")
+        target = result.get("target")
+        
+        seq_len = None
+        if "seq_len" in result.keys():
+            seq_len = result.get("seq_len")
+
+        if seq_len is not None and target.dim() > 1:
+            max_len = target.size(1)
+            masks = seq_len_to_mask(seq_len=seq_len, max_len=max_len)
+        else:
+            masks = None
+        if pred.dim() == target.dim():
+            if torch.numel(pred) !=torch.numel(target):
+                raise RuntimeError(f"when pred have same dimensions with target, they should have same element numbers."
+                                   f" while target have shape:{target.shape}, "
+                                   f"pred have shape: {pred.shape}")
+
+            pass
+        elif pred.dim() == target.dim() + 1:
+            pred = pred.argmax(dim=-1)
+            if seq_len is None and target.dim() > 1:
+                warnings.warn("You are not passing `seq_len` to exclude pad when calculate accuracy.")
+        else:
+            raise RuntimeError(f"when pred have size:{pred.shape}, target should have size: {pred.shape} or "
+                               f"{pred.shape[:-1]}, got {target.shape}.")
+
+        if masks is not None:
+            self.correct += torch.sum(torch.eq(pred, target).masked_fill(masks.eq(False), 0)).item()
+            self.total += torch.sum(masks).item()
+        else:
+            self.correct += torch.sum(torch.eq(pred, target)).item()
+            self.total += np.prod(list(pred.size()))
---
+++ tests/metrics/test_accuracy.py
@@ -0,0 +1,79 @@
+import sys
+sys.path.append("/mnt/petrelfs/hongjiawei/collie/")
+import pytest
+from collie.metrics.accuracy import Accuracy
+import torch
+
+class TestAccuracy:
+
+    def test_demo1(self):
+        """
+            测试输入为[B, n_classes] 的 pred 和 [B] 的 target
+        """
+        input_result = {'pred': torch.tensor([[0.2, 0.4, 0.3, 0.1], [0.8, 0.5, 0.4, 0.3],
+                                              [0.6, 0.1, 0.2, 0.3], [0.3, 0.6, 0.65, 0.5]]),
+                       'target': torch.tensor([0, 0, 0, 0])}
+        metric = Accuracy()
+        
+        metric.update(input_result)
+        metric_result = metric.get_metric()
+        print(metric_result)
+        assert metric_result == {'acc': 0.5, 'total': 4, 'correct': 2}
+
+    def test_demo2(self):
+        """
+            测试输入为 pred 的 shape > target 的 shape
+        """
+        input_result = {'pred': torch.zeros(4, 3, 2),
+                        'target': torch.zeros(4)}
+        metric = Accuracy()
+
+        with pytest.raises(RuntimeError) as e:
+            metric.update(input_result)
+        exec_msg = e.value.args[0]
+        print(exec_msg)
+        assert exec_msg == 'when pred have size:torch.Size([4, 3, 2]), target should have size: torch.Size([4, 3, 2]) or torch.Size([4, 3]), got torch.Size([4]).'
+
+    def test_demo3(self):
+        """
+            测试输入带冗余的参数
+        """
+        input_result = {'pred': torch.tensor([[0.2, 0.4, 0.3, 0.1], [0.8, 0.5, 0.4, 0.3],
+                                              [0.6, 0.1, 0.2, 0.3], [0.7, 0.6, 0.65, 0.5]]),
+                         'target': torch.tensor([0, 0, 0, 0]),
+                         'unused': 1}
+        metric = Accuracy()
+        
+        metric.update(input_result)
+        metric_result = metric.get_metric()
+        print(metric_result)
+        assert metric_result == {'acc': 0.75, 'total': 4, 'correct': 3}
+
+    def test_demo4(self):
+        input_result = {'pred': torch.tensor([  [[0.1831, 0.3912],
+                                                [0.6588, 0.1285],
+                                                [0.9351, 0.0895]],
+
+                                                [[0.9126, 0.4580],
+                                                [0.0580, 0.9839],
+                                                [0.2595, 0.6489]],
+
+                                                [[0.4953, 0.3380],
+                                                [0.0509, 0.4378],
+                                                [0.6848, 0.4253]],
+
+                                                [[0.8520, 0.4735],
+                                                [0.1479, 0.6368],
+                                                [0.6576, 0.9255]]]),
+                         'target': torch.tensor([[1, 0, 0],
+                                                 [1, 0, -1],
+                                                 [0, 1, -1],
+                                                 [1, -1, -1]]),
+                         'seq_len': torch.tensor([3, 2, 2, 1])}
+
+        metric = Accuracy()
+        
+        metric.update(input_result)
+        metric_result = metric.get_metric()
+        print(metric_result)
+        assert metric_result == {'acc': 0.625, 'total': 8, 'correct': 5}
