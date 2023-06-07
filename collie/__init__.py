9a259f0f717c8aef53384a0471dd07c0732d504b
x54-729 <17307130121@fudan.edu.cn>
2023-06-07 20:22:32 +0800

    Add modules to __all__ in collie/__init__.py

--- collie/__init__.py
+++ collie/__init__.py
@@ -1,20 +1,48 @@
 ''' **CoLLie** 为 **Causal Language Modeling** 提供了一系列的模型和工具，支持分布式训练和验证的快速部署
 '''
+from .trainer import Trainer
+from .config import CollieConfig
 from .models import LlamaForCausalLM, MossForCausalLM, CollieModelForCausalLM, ChatGLMForCausalLM
+from .callbacks import Callback, HasMonitorCallback, CheckpointCallback
+from .module import PipelineGenerationMixin, ColumnParallelLinear, \
+    RowParallelLinear, VocabParallelEmbedding, RowParallelLinearWithoutBias, \
+    LinearWithHiddenStates, ColumnParallelLMHead, GPTLMLoss
 from .utils import progress, setup_distribution, set_seed, env, \
     setup_ds_engine, zero3_load_state_dict, is_zero3_enabled, \
     broadcast_tensor, find_tensors, BaseProvider, GradioProvider, \
     BaseMonitor, StepTimeMonitor, TGSMonitor, MemoryMonitor, LossMonitor, \
     EvalMonitor
-from .module import PipelineGenerationMixin, ColumnParallelLinear, RowParallelLinearWithoutBias, LinearWithHiddenStates, ColumnParallelLMHead, GPTLMLoss
-from .trainer import Trainer
-from .config import CollieConfig
 
 __all__ = [
+    # train
+    'Trainer',
+    'Evaluator',
+
+    # config
+    'CollieConfig',
+
+    # models
     'LlamaForCausalLM',
     'MossForCausalLM',
     'CollieModelForCausalLM',
     'ChatGLMForCausalLM',
+
+    # modules
+    'PipelineGenerationMixin',
+    'ColumnParallelLinear',
+    'RowParallelLinear',
+    'VocabParallelEmbedding',
+    'RowParallelLinearWithoutBias',
+    'LinearWithHiddenStates',
+    'ColumnParallelLMHead',
+    'GPTLMLoss',
+
+    # callbacks
+    'Callback',
+    'CheckpointCallback',
+    'HasMonitorCallback',
+
+    # utils
     'progress',
     'setup_distribution',
     'set_seed',
@@ -24,12 +52,6 @@ __all__ = [
     'is_zero3_enabled',
     'broadcast_tensor',
     'find_tensors',
-    'PipelineGenerationMixin',
-    'ColumnParallelLinear',
-    'RowParallelLinearWithoutBias',
-    'LinearWithHiddenStates',
-    'ColumnParallelLMHead',
-    'GPTLMLoss',
     'BaseProvider', 
     'GradioProvider', 
     'Trainer',
