3f3a2ca0dd20f70618651484535099de1047fad6
zhangshuo <zhangshuolove@live.com>
2023-06-23 19:24:22 +0800

    fix: bugs in rtd

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,8 +14,8 @@
 import os
 import sys
 
-# sys.path.insert(0, os.path.abspath('../../'))
-# sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../../'))
+print(sys.path)
 
 import pytorch_sphinx_theme
 
