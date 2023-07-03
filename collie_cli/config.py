39008c7c1bdbaa6e941d864ab7758d67482624da
KYLN24 <hlguo20@fudan.edu.cn>
2023-07-03 21:18:37 +0800

    feat: able to create a deepspeed config

--- collie_cli/config.py
+++ collie_cli/config.py
@@ -44,9 +44,6 @@ _prompt_argname_map = {
     "Batch size for evaluation": "eval_batch_size",
     "Do you want to use the gradient checkpointing?": "checkpointing",
     "Do you want to use FlashAttention?": "use_flash",
-    "The possibility for dropout": "dropout",
-    "Choose the method for parameter initialization": "initization_method",  # TODO: a spelling mistake?
-    "Path to the configuration file for DeepSpeed": "ds_config",  # TODO: choose between using an existing config or to generate a new one.
 }
 
 
@@ -59,6 +56,130 @@ def _parse(v):
         return v == "Yes"
     else:
         return v
+
+
+def generate_deepspeed_config(
+    word_color,
+    train_micro_batch_size_per_gpu,
+    gradient_accumulation_steps,
+):
+    print("\n")
+    use_existing_config = (
+        Bullet(
+            "Do you have an existing DeepSpeed config?",
+            choices=[
+                "Yes",
+                "No",
+            ],
+            bullet="> ",
+            word_color=word_color,
+        ).launch()
+        == "Yes"
+    )
+
+    config = {}
+    if use_existing_config:
+        print("\n")
+        config = Input("Path to the DeepSpeed config", default="ds_config.yml").launch()
+    else:
+        print("\nPlease follow the instructions to generate a new DeepSpeed config.")
+
+        # Precision
+        precision = Bullet(
+            "\nSelect the training precision",
+            choices=["defualt", "fp16 (required for ZeRO)", "bf16"],
+            bullet="> ",
+            word_color=word_color,
+        ).launch()
+        if precision == "bf16":
+            config["bf16"] = {"enabled": True}
+        elif precision == "fp16 (required for ZeRO)":
+            config["fp16"] = {
+                "enabled": True,
+            }
+            config["fp16"]["auto_cast"] = (
+                Bullet(
+                    "\nDo you want to enable auto-cast?",
+                    choices=["Yes", "No"],
+                    bullet="> ",
+                    word_color=word_color,
+                ).launch()
+                == "Yes"
+            )
+            # ZeRO
+            if (
+                Bullet(
+                    "\nDo you want to use ZeRO memory optimizations?",
+                    choices=["Yes", "No"],
+                    bullet="> ",
+                    word_color=word_color,
+                ).launch()
+                == "Yes"
+            ):
+                zero_config = {}
+
+                zero_config["stage"] = Bullet(
+                    "\nSelect the ZeRO stage",
+                    choices=["0", "1", "2", "3"],
+                    bullet="> ",
+                    word_color=word_color,
+                ).launch()
+
+                # Offload parameters
+                if (
+                    zero_config["stage"] == "3"
+                    and Bullet(
+                        "\nDo you want to offload parameters?",
+                        choices=["Yes", "No"],
+                        bullet="> ",
+                        word_color=word_color,
+                    ).launch()
+                    == "Yes"
+                ):
+                    offload_param = {}
+                    offload_param["device"] = Bullet(
+                        "\nDevice: ",
+                        choices=["cpu", "nvme"],
+                        bullet="> ",
+                        word_color=word_color,
+                    ).launch()
+                    if offload_param["device"] == "nvme":
+                        offload_param["nvme_path"] = Input(
+                            "\nOffload path", word_color=word_color
+                        ).launch()
+                    zero_config["offload_param"] = offload_param
+
+                # Offload optimizer states
+                if (
+                    zero_config["stage"] != "0"
+                    and Bullet(
+                        "\nDo you want to offload optimizer states?",
+                        choices=["Yes", "No"],
+                        bullet="> ",
+                        word_color=word_color,
+                    ).launch()
+                    == "Yes"
+                ):
+                    offload_optimizer = {}
+                    offload_optimizer["device"] = (
+                        Bullet(
+                            "\nDevice: ",
+                            choices=["cpu", "nvme"],
+                            bullet="> ",
+                            word_color=word_color,
+                        ).launch()
+                        if zero_config["stage"] == "3"
+                        else "cpu"
+                    )
+                    if offload_optimizer["device"] == "nvme":
+                        offload_optimizer["nvme_path"] = Input(
+                            "\nOffload path", word_color=word_color
+                        ).launch()
+                    zero_config["offload_optimizer"] = offload_optimizer
+
+                config["zero_optimization"] = zero_config
+
+    return config
 
 
 def config_command_entry(args):
@@ -89,7 +210,7 @@ def config_command_entry(args):
                     "Yes",
                     "No",
                 ],
-                bullet=" >",
+                bullet="> ",
                 word_color=word_color,
             ),
             Bullet(
@@ -98,60 +219,59 @@ def config_command_entry(args):
                     "Yes",
                     "No",
                 ],
-                bullet=" >",
+                bullet="> ",
                 word_color=word_color,
             ),
-            Input("The possibility for dropout", default="0.0", word_color=word_color),
-            Bullet(
-                "Choose the method for parameter initialization",
-                choices=[
-                    "normal",
-                    "xavier_normal",
-                    "xavier_uniform",
-                    "kaiming_normal",
-                    "kaiming_uniform",
-                    "orthogonal",
-                    "sparse",
-                    "eye",
-                    "dirac",
-                ],
-                bullet=" >",
-                word_color=word_color,
-            ),
-            Input(
-                "Path to the configuration file for DeepSpeed",
-                default="ds_config.yml",
-                word_color=word_color,
-            ),
+            # Input("The possibility for dropout", default="0.0", word_color=word_color),
+            # Bullet(
+            #     "Choose the method for parameter initialization",
+            #     choices=[
+            #         "normal",
+            #         "xavier_normal",
+            #         "xavier_uniform",
+            #         "kaiming_normal",
+            #         "kaiming_uniform",
+            #         "orthogonal",
+            #         "sparse",
+            #         "eye",
+            #         "dirac",
+            #     ],
+            #     bullet="> ",
+            #     word_color=word_color,
+            # ),
+            # Input(
+            #     "Path to the configuration file for DeepSpeed",
+            #     default="ds_config.yml",
+            #     word_color=word_color,
+            # ),
             Input("Size for pipeline parallelism", default="1", word_color=word_color),
             Input("Size for tensor parallelism", default="1", word_color=word_color),
             Input("Size for data paralellism", default="1", word_color=word_color),
             Bullet(
                 "Choose the partition method for pipeline parallelism.",
                 choices=["parameters", "uniform", "type:[regex]"],
-                bullet=" >",
+                bullet="> ",
                 margin=2,
                 word_color=word_color,
             ),
         ]
     )
 
-    regx_cli = VerticalPrompt(
-        [
-            Input(
-                "You've selected the type:[regex] method, please enter the regex",
-                default="",
-                word_color=word_color,
-            )
-        ],
-    )
-
     result = config_command_cli.launch()
     config = {_prompt_argname_map[k]: _parse(v) for k, v in result}
 
     if config["pp_partition_method"] == "type:[regex]":
-        regx_result = regx_cli.launch()
-        config["pp_partition_method"] = regx_result[0][1]
+        config["pp_partition_method"] = Input(
+            "You've selected the type:[regex] method, please enter the regex",
+            default="",
+            word_color=word_color,
+        ).launch()
+
+    config["ds_config"] = generate_deepspeed_config(
+        word_color,
+        config["train_micro_batch_size"],
+        config["gradient_accumulation_steps"],
+    )
 
     with open(args.config_file, "w") as f:
         yaml.dump(config, f, Dumper=yaml.SafeDumper)
