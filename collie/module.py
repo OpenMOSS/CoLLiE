281979d09abedda9dfcdb28a3e8753b9418b1394
Kai Lv <39761308+KaiLv69@users.noreply.github.com>
2023-08-10 15:40:07 +0800

    Revert "split 65b myself"
    This reverts commit bb7613dc33ade11783051865720872bf6a18589f.

--- collie/module.py
+++ collie/module.py
@@ -539,61 +539,6 @@ class PipelineModel(PipelineModule, PipelineGenerationMixin):
         self.forward_type = "train" # train, eval, generate
         self.skip_input_embedding()
 
-    def _partition_layers(self, method='uniform'):
-        num_stages = self._topo.get_dim('pipe')
-        stage_id = self._topo.get_coord(self.global_rank).pipe
-
-        if self.global_rank == 0:
-            logger.info(f'Partitioning pipeline stages with method {method}')
-
-        method = method.lower()
-
-        # Each stage gets a simple uniform number of layers.
-        if method == 'uniform':
-            self.parts = [0, 2, 5, 7, 10, 12, 15, 17, 20, 22, 25, 27, 30, 32, 35, 38, 41, 44, 47, 50, 52, 55, 57, 60,
-                          62, 65, 67, 70, 72, 75, 77, 79, 83]
-            # num_layers = len(self._layer_specs)
-            # self.parts = ds_utils.partition_uniform(num_items=num_layers, num_parts=num_stages)
-        elif method == 'parameters':
-            param_counts = self._count_layer_params()
-            self.parts = ds_utils.partition_balanced(weights=param_counts, num_parts=num_stages)
-        elif method.startswith('type:'):
-            layertype = method.split(':')[1]
-            binary_weights = [0] * len(self._layer_specs)
-            for idx in self._find_layer_type(layertype):
-                binary_weights[idx] = 1
-            self.parts = ds_utils.partition_balanced(weights=binary_weights, num_parts=num_stages)
-        elif method == 'profile':
-            raise NotImplementedError(f'Partitioning method {method} not implemented.')
-        else:
-            raise NotImplementedError(f'Partitioning method {method} not implemented.')
-
-        # Print some information on the partitioning.
-        if self.global_rank == 0:
-            for stage in range(num_stages):
-                start = self.parts[stage]
-                stop = self.parts[stage + 1]
-                print(f'stage={stage} layers={stop - start}')
-                for idx, layer in enumerate(self._layer_specs[start:stop]):
-                    name = str(layer)
-                    if isinstance(layer, LayerSpec):
-                        name = layer.typename.__name__
-                    if isinstance(layer, nn.Module):
-                        name = layer.__class__.__name__
-                    else:
-                        try:
-                            name = layer.__name__
-                        except AttributeError:
-                            pass
-                    print(f'    {idx+start:2d}: {name}')
-            if self.loss_fn:
-                try:
-                    print(f'  loss: {self.loss_fn.__name__}')
-                except AttributeError:
-                    print(f'  loss: {self.loss_fn.__class__.__name__}')
-
-        self._set_bounds(start=self.parts[stage_id], stop=self.parts[stage_id + 1])
-
     def _flatten_layers(self, layers):
         # layers: list of tuple/layer
         _layers = []
