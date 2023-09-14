0bdf08412d43a90a11c0e801fc7943fc430b8cbf
KYLN24 <1296845690@qq.com>
2023-09-14 14:34:57 +0800

    style: format by black

--- collie/metrics/accuracy.py
+++ collie/metrics/accuracy.py
@@ -1,10 +1,10 @@
-from typing import Dict, Optional, List
+from typing import Dict, List, Optional
 
-import torch
 import numpy as np
+import torch
 
-from collie.metrics.base import BaseMetric
 from collie.log import logger
+from collie.metrics.base import BaseMetric
 from collie.utils.seq_len_to_mask import seq_len_to_mask
 
 
@@ -15,11 +15,11 @@ class AccuracyMetric(BaseMetric):
     :param gather_result: 在计算 metric 的时候是否自动将各个进程上的输入进行聚合后再输入到 update 之中。
     """
 
-    def __init__(self, gather_result: bool=False):
+    def __init__(self, gather_result: bool = False):
         super().__init__(gather_result=gather_result)
         self.correct = 0
         self.total = 0
-    
+
     def reset(self):
         """
         重置参数
@@ -27,7 +27,7 @@ class AccuracyMetric(BaseMetric):
         self.correct = 0
         self.total = 0
 
-    def get_metric(self)->Dict:
+    def get_metric(self) -> Dict:
         r"""
         :meth:`get_metric` 函数将根据 :meth:`update` 函数累计的评价指标统计量来计算最终的评价结果。
 
@@ -37,11 +37,9 @@ class AccuracyMetric(BaseMetric):
         """
         # evaluate_result = {'acc': round(self.correct / (self.total + 1e-12), 6),
         #                    'total': self.total, 'correct': self.correct}
-        return {
-            "acc": round(self.correct / (self.total + 1e-12), 6)
-        }
-    
-    def update(self, result:Dict):
+        return {"acc": round(self.correct / (self.total + 1e-12), 6)}
+
+    def update(self, result: Dict):
         r"""
         :meth:`update` 函数将针对一个批次的预测结果做评价指标的累计。
 
@@ -54,14 +52,16 @@ class AccuracyMetric(BaseMetric):
             * seq_len - 序列长度标记, 标记的形状可以是 ``None``,  或者 ``torch.Size([B])`` 。
               如果 mask 也被传进来的话 ``seq_len`` 会被忽略
         """
-        assert "pred" in result and "target" in result, "pred and target  must in result, but they not."
+        assert (
+            "pred" in result and "target" in result
+        ), "pred and target  must in result, but they not."
         pred = result.get("pred")
         target = result.get("target")
-        
-        # ddp 时候需要手动 gahter 所有数据。 默认输入的类型都是tensor
+
+        # ddp 时候需要手动 gahter 所有数据。 默认输入的类型都是 tensor
         if isinstance(pred, List):
             pred = torch.stack(pred, dim=0)
-        
+
         if isinstance(target, List):
             target = torch.stack(target, dim=0)
 
@@ -75,22 +75,30 @@ class AccuracyMetric(BaseMetric):
         else:
             masks = None
         if pred.dim() == target.dim():
-            if torch.numel(pred) !=torch.numel(target):
-                raise RuntimeError(f"when pred have same dimensions with target, they should have same element numbers."
-                                   f" while target have shape:{target.shape}, "
-                                   f"pred have shape: {pred.shape}")
+            if torch.numel(pred) != torch.numel(target):
+                raise RuntimeError(
+                    f"when pred have same dimensions with target, they should have same element numbers."
+                    f" while target have shape:{target.shape}, "
+                    f"pred have shape: {pred.shape}"
+                )
 
             pass
         elif pred.dim() == target.dim() + 1:
             pred = pred.argmax(dim=-1)
             if seq_len is None and target.dim() > 1:
-                logger.warning("You are not passing `seq_len` to exclude pad when calculate accuracy.")
+                logger.warning(
+                    "You are not passing `seq_len` to exclude pad when calculate accuracy."
+                )
         else:
-            raise RuntimeError(f"when pred have size:{pred.shape}, target should have size: {pred.shape} or "
-                               f"{pred.shape[:-1]}, got {target.shape}.")
+            raise RuntimeError(
+                f"when pred have size:{pred.shape}, target should have size: {pred.shape} or "
+                f"{pred.shape[:-1]}, got {target.shape}."
+            )
 
         if masks is not None:
-            self.correct += torch.sum(torch.eq(pred, target).masked_fill(masks.eq(False), 0)).item()
+            self.correct += torch.sum(
+                torch.eq(pred, target).masked_fill(masks.eq(False), 0)
+            ).item()
             self.total += torch.sum(masks).item()
         else:
             self.correct += torch.sum(torch.eq(pred, target)).item()
