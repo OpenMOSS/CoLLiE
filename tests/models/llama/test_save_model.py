c2e8155af3f74076fa208a477a816ad3f402bf98
Kai Lv <39761308+KaiLv69@users.noreply.github.com>
2024-01-01 17:47:17 +0800

    add: test model save

--- tests/models/llama/test_save_model.py
+++ tests/models/llama/test_save_model.py
@@ -5,7 +5,7 @@ from transformers import AutoTokenizer, GenerationConfig, AutoModel
 from collie.models import ChatGLM2ForCausalLM, LlamaForCausalLM
 from collie import CollieConfig, env, Trainer
 
-pretrained_path = "THUDM/chatglm2-6b"
+pretrained_path = "huggyllama/llama-7b"
 
 # tokenizer = AutoTokenizer.from_pretrained(
 #         pretrained_path,
@@ -13,10 +13,14 @@ pretrained_path = "THUDM/chatglm2-6b"
 #     )
 config = CollieConfig.from_pretrained(pretrained_path,
         trust_remote_code=True)
-config.tp_size = 1
-config.pp_size = 2
-model = ChatGLM2ForCausalLM.from_pretrained(pretrained_path, config=config)
+config.tp_size = 2
+config.pp_size = 4
+model = LlamaForCausalLM.from_pretrained(pretrained_path, config=config).cuda()
+
 trainer = Trainer(model, config=config)
 trainer.save_model("./dev")
+
+# model.save_parallel_state_dict(model.state_dict(), "./dev", config=config)
+
 print("start loading")
 model = AutoModel.from_pretrained("./dev", trust_remote_code=True)
