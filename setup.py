bcdb9e45f7161711458587811e3465b1d70f25ae
Kai Lv <39761308+KaiLv69@users.noreply.github.com>
2024-01-03 11:34:27 +0800

    update description

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("requirements.txt", encoding="utf-8") as f:
 setup(
     name="collie-lm",
     version="1.0.5",
-    description="CoLLiE: Collaborative Tuning of Large Language Models in an Efficient Way",
+    description="CoLLiE: Collaborative Training of Large Language Models in an Efficient Way",
     author="OpenLMLab",
     author_email="yanhang@pjlab.org.cn",
     packages=find_packages(),
