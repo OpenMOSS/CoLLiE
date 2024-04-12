40d322a3c2b57a943739c4e1a272dd2b69958525
4AKker <39761308+KaiLv69@users.noreply.github.com>
2024-04-12 22:53:11 +0800

    add moss template

--- collie/data/template_utils.py
+++ collie/data/template_utils.py
@@ -1,21 +1,30 @@
+import json
 from typing import Callable
 
 
-def prepare_chatml_messages(messages, special_tokens_map, add_generation_prompt=False):
+def prepare_chatml_messages(messages, special_tokens_map, text_field, add_generation_prompt=False):
     """
-    ChatML Template
+    准备ChatML格式的多轮对话，其格式如下：
 
-    {{ bos_token }}
-    {% for message in messages %}
-        {{'<|im_start|>' + message['role'] + '\n' + message['content'] + '<|im_end|>' + '\n'}}
-    {% endfor %}
-    {% if add_generation_prompt %}
-        {{ '<|im_start|>assistant\n' }}
-    {% endif %}
+        {{ bos_token }}
+        {% for message in messages %}
+            {{'<|im_start|>' + message['role'] + '\n' + message['content'] + '<|im_end|>' + '\n'}}
+        {% endfor %}
+        {% if add_generation_prompt %}
+            {{ '<|im_start|>assistant\n' }}
+        {% endif %}
+
+    :param messages: 包含对话消息的字典，每个消息包含角色和内容字段。
+    :param special_tokens_map: 特殊token映射，包含'bos_token'等特殊token。
+    :param text_field: 对话数据中文本字段的名称。
+    :param add_generation_prompt: 是否添加用于生成的提示，默认为False。
+
+    :return: 返回一个列表，其中每个元素是一个字典，包含'content'和'require_loss'两个字段。'content'字段表示消息内容，
+             'require_loss'字段表示是否需要计算该消息的损失。
     """
     prepared_messages = []
     prepared_messages += [{"content": special_tokens_map['bos_token'], "require_loss": False}]
-    for message in messages['history']:
+    for message in messages[text_field]:
         if message['role'] == "assistant":
             prepared_messages += [{"content": '<|im_start|>assistant\n', "require_loss": False}]
             prepared_messages += [{"content": message['content'] + '<|im_end|>', "require_loss": True}]
@@ -29,8 +38,72 @@ def prepare_chatml_messages(messages, special_tokens_map, add_generation_prompt=
     return prepared_messages
 
 
-def prepare_moss_messages(messages, special_tokens_map, add_generation_prompt=False):
-    pass
+def prepare_moss_messages(messages, special_tokens_map, text_field, add_generation_prompt=False):
+    """
+    准备MOSS格式的多轮对话，其格式如下：
+
+        {{ bos_token }}
+        {% for message in messages %}
+
+
+    """
+    prepared_messages = []
+    prepared_messages += [{"content": special_tokens_map['bos_token'], "require_loss": False}]
+    end_token_dict = {
+        "user": "<|end_of_user|>",
+        "system": "<|end_of_system|>",
+        "assistant": "<|end_of_assistant|>",
+        "func_call": "<|end_of_func_call|>",
+        "func_ret": "<|end_of_func_ret|>",
+        "thought": "<|end_of_thought|>",  # 暂时没用
+        "image": "<|end_of_image|>",  # 暂时没用
+        "audio": "<|end_of_audio|>",  # 暂时没用
+        "video": "<|end_of_video|>",  # 暂时没用
+        "moss": "<|end_of_moss|>",  # 每轮对话的最终回复
+    }
+    first_user_message = True
+
+    for message in messages[text_field]:
+        role = message['role']
+        if role == "user":
+            message_content = f"<|im_start|>user\n{message['content']}{end_token_dict['user']}\n"
+            if first_user_message:
+                prepared_messages.append({"content": message_content, "require_loss": False})
+                first_user_message = False
+            else:
+                # 上一轮 moss 完成了回复，预测的最后一个 token 是 <|end_of_moss|>
+                prepared_messages.append({"content": end_token_dict['moss'], "require_loss": True})
+                prepared_messages.append({"content": f"\n{message_content}", "require_loss": False})
+        elif role == "assistant":
+            prepared_messages.append({"content": '<|im_start|>assistant\n', "require_loss": False})
+            prepared_messages.append({"content": message['content'] + end_token_dict['assistant'], "require_loss": True})
+            prepared_messages.append({"content": '\n', "require_loss": False})
+        elif role == "func_call":
+            func_call_content = json.dumps(message["func_call"])
+            prepared_messages.append(
+                {
+                    "content": f"<|im_start|>func_call\n{func_call_content}{end_token_dict['func_call']}",
+                    "require_loss": True
+                }
+            )
+            prepared_messages.append({"content": '\n', "require_loss": False})
+        elif role == "func_ret":
+            func_ret_content = json.dumps(message["func_ret"])
+            prepared_messages.append(
+                {
+                    "content": f"<|im_start|>func_ret\n{func_ret_content}{end_token_dict['func_ret']}\n",
+                    "require_loss": False
+                }
+            )
+        else:
+            prepared_messages.append(
+                {"content": f"<|im_start|>{role}\n{message['content']}{end_token_dict[role]}\n", "require_loss": False}
+            )
+
+    prepared_messages.append({"content": end_token_dict['moss'] + "\n", "require_loss": True})
+    if add_generation_prompt:
+        prepared_messages += [{"content": '<|im_start|>assistant\n', "require_loss": False}]
+    return prepared_messages
 
 
 TOKENIZER_PREPARE_TEMPLATE_FN_MAPPING = {
@@ -39,15 +112,19 @@ TOKENIZER_PREPARE_TEMPLATE_FN_MAPPING = {
 }
 
 
-def tokenize_conversation(conversation, tokenizer, prepare_template_fn: Callable or None = None,
+def tokenize_conversation(conversation, tokenizer, text_field='history', prepare_template_fn: Callable or None = None,
                           add_generation_prompt=False):
     if prepare_template_fn is None:
         if type(tokenizer).__name__ not in TOKENIZER_PREPARE_TEMPLATE_FN_MAPPING:
             raise ValueError(f"Tokenizer {type(tokenizer).__name__} has no preset template. Please provide one.")
         else:
             prepare_template_fn = TOKENIZER_PREPARE_TEMPLATE_FN_MAPPING[type(tokenizer).__name__]
-    prepared_messages = prepare_template_fn(messages=conversation, special_tokens_map=tokenizer.special_tokens_map,
-                                            add_generation_prompt=add_generation_prompt)
+    prepared_messages = prepare_template_fn(
+        messages=conversation,
+        special_tokens_map=tokenizer.special_tokens_map,
+        text_field=text_field,
+        add_generation_prompt=add_generation_prompt
+    )
 
     input_ids = []
     labels = []
