6182f7f90756041614f214cd5025de7b81134a07
Kai Lv <39761308+KaiLv69@users.noreply.github.com>
2024-04-27 22:21:55 +0800

    refactor(example): refactor examples (#176)

--- examples/downstream_task/finetune_llama_for_translation.py
+++ examples/downstream_task/finetune_llama_for_translation.py
@@ -1,29 +1,30 @@
 import sys
-sys.path.append("..")
-import torch
-from datasets import load_dataset
-from transformers import AutoTokenizer, GenerationConfig
-from functools import partial
-from collie import Trainer, EvaluatorForPerplexity, ChatGLMForCausalLM, CollieConfig, PPLMetric, AccuracyMetric, DecodeMetric, CollieDatasetForTraining, CollieDatasetForGeneration, \
+sys.path.append("../..")
+
+from rich.traceback import install
+from collie import Trainer, EvaluatorForPerplexity, LlamaForCausalLM, CollieConfig, PPLMetric, AccuracyMetric, DecodeMetric, CollieDatasetForTraining, CollieDatasetForGeneration, \
     LossMonitor, TGSMonitor, MemoryMonitor, EvalMonitor, GradioProvider, EvaluatorForGeneration, LRMonitor, BleuMetric, DashProvider
-config = CollieConfig.from_pretrained("THUDM/chatglm-6b", trust_remote_code=True)
+    
+config = CollieConfig.from_pretrained("decapoda-research/llama-7b-hf")
 config.pp_size = 8
 config.train_micro_batch_size = 1
 config.eval_batch_size = 1
 config.gradient_accumulation_steps = 128
 config.eval_per_n_epochs = 1
 config.train_epochs = 10
+config.use_flash = True
 config.ds_config = {
     "fp16": {
         "enabled": True
     },
     "monitor_config": {
         "enabled": True,
+        "tag": "v2",
         "wandb": {
             "enabled": True,
             "team": "00index",
-            "project": "collie",
-            "group": "translation"
+            "project": "collie-experiment",
+            "group": "flash-attention-tp"
         }
     }
 }
@@ -31,8 +32,8 @@ config.seed = 1024
 # Prepare training dataset
 train_dataset = [
     {
-        "input": f"[Round {0}]\n问：把下面这句话从法语翻译成英语. {sample['translation']['fr']}\n答：[gMASK]<sop>",
-        "output": f"{sample['translation']['en']}<eop>"
+        "input": f"<s>Translate from French to English. French: {sample['translation']['fr']} English: ",
+        "output": f"{sample['translation']['en']}</s>"
     } for sample in load_dataset("iwslt2017", name="iwslt2017-fr-en", split="train[:8000]")
 ]
 # Prepare perplexity evaluation dataset
@@ -42,28 +43,24 @@ eval_dataset_ppl, train_dataset = train_dataset[:int(
 # Prepare classification evaluation dataset
 eval_dataset_bleu = [
     {
-        "text": f"[Round {0}]\n问：把下面这句话从法语翻译成英语. {sample['translation']['fr']}\n答：[gMASK]<sop>",
-        "target": " ".join(f"{sample['translation']['en']}<eop>".split())
+        "text": f"<s>Translate from French to English. French: {sample['translation']['fr']} English: ",
+        "target": " ".join(f"{sample['translation']['en']}</s>".split())
     } for sample in load_dataset("iwslt2017", name="iwslt2017-fr-en", split="train[100:150]")
 ]
 # Prepare model
-model = ChatGLMForCausalLM.from_pretrained("THUDM/chatglm-6b", config=config)
+model = LlamaForCausalLM.from_pretrained("decapoda-research/llama-7b-hf", config=config)
 optimizer = torch.optim.AdamW(model.parameters(), lr=2e-5)
-# lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
-#     optimizer=optimizer, T_max=config.train_epochs * len(train_dataset) / (config.train_micro_batch_size * config.gradient_accumulation_steps))
 lr_scheduler = torch.optim.lr_scheduler.StepLR(
     optimizer=optimizer, step_size=1, gamma=0.9
 )
-tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm-6b", trust_remote_code=True)
-# 默认的tokenizer不会把[gMASK]当作一个token，所以需要手动添加
-tokenizer.unique_no_split_tokens.append("[gMASK]")
+tokenizer = LlamaTokenizer.from_pretrained("decapoda-research/llama-7b-hf", add_eos_token=False, add_bos_token=False)
 # Convert to CoLLie Dataset
-traine_dataset = CollieDatasetForTraining(train_dataset,
-                                          tokenizer=partial(tokenizer, add_special_tokens=False))
+train_dataset = CollieDatasetForTraining(train_dataset,
+                                         tokenizer=tokenizer)
 eval_dataset_ppl = CollieDatasetForTraining(eval_dataset_ppl,
-                                            tokenizer=partial(tokenizer, add_special_tokens=False))
+                                            tokenizer=tokenizer)
 eval_dataset_bleu = CollieDatasetForGeneration(eval_dataset_bleu,
-                                               tokenizer=partial(tokenizer, add_special_tokens=False))
+                                               tokenizer=tokenizer)
 # Prepare Evaluator
 evaluator_ppl = EvaluatorForPerplexity(
     model=model,
@@ -93,6 +90,12 @@ evaluator_bleu = EvaluatorForGeneration(
         max_new_tokens=100,
     )
 )
+server = Server(model=model, data_provider=GradioProvider(tokenizer, stream=True,
+                                                          generation_config=GenerationConfig(
+                                                              eos_token_id=tokenizer.eos_token_id,
+                                                              pad_token_id=tokenizer.pad_token_id,
+                                                              max_new_tokens=100,
+                                                          )))
 # Prepare Trainer
 trainer = Trainer(
     model=model,
@@ -100,13 +103,15 @@ trainer = Trainer(
     lr_scheduler=lr_scheduler,
     config=config,
     optimizer=optimizer,
-    train_dataset=traine_dataset,
+    train_dataset=train_dataset,
     monitors=[
         LossMonitor(config),
         TGSMonitor(config),
         MemoryMonitor(config),
         LRMonitor(config)
     ],
+    server=server,
     evaluators=[evaluator_ppl, evaluator_bleu]
 )
 trainer.train()
+# trainer.save_checkpoint(path="/mnt/petrelfs/zhangshuo/model/test_save_checkpoint", mode="model")
