86445698f75f94acdad180216993d2eaa1537954
LinqiY <100989140+LinqiY@users.noreply.github.com>
2024-05-18 09:27:21 +0800

    Update __init__.py

--- collie/models/__init__.py
+++ collie/models/__init__.py
@@ -6,3 +6,4 @@ from .internlm import InternLMForCausalLM
 from .chatglm2 import ChatGLM2ForCausalLM
 from .moss_moon import Moss003MoonForCausalLM
 from .internlm2 import InternLM2ForCausalLM
+from .mistral import MistralForCausalLM
