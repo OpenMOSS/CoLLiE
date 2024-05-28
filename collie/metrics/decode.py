a92d983822eed64c7c1d96087ab8b6808ff824d4
LinqiY <100989140+LinqiY@users.noreply.github.com>
2024-05-28 16:45:27 +0800

    modify decodemetric class

--- collie/metrics/decode.py
+++ collie/metrics/decode.py
@@ -1,8 +1,12 @@
+import json
 from typing import Any, Dict
 from collie.metrics.base import BaseMetric
-from collie.utils import env
+# from collie.utils import env
+from collie.metrics.env import env
 from collie.log.logger import logger
 import torch
+import os
+
 
 class DecodeMetric(BaseMetric):
     """
@@ -21,6 +25,21 @@ class DecodeMetric(BaseMetric):
         self.verbose = verbose
         self.save_to_file = save_to_file
         self.save_path = save_path
+
+        # 确保目录存在
+        if self.save_to_file and self.save_path:
+            directory = os.path.dirname(self.save_path)
+            os.makedirs(directory, exist_ok=True)
+
+            # 检查文件是否存在并创建文件
+            if not os.path.exists(self.save_path):
+                print(f"文件 {self.save_path} 不存在，将创建新文件。")
+                # 创建空文件
+                with open(self.save_path, 'w') as file:
+                    file.write("")  
+                print(f"已创建文件 {self.save_path}。")
+            else:
+                print(f"文件 {self.save_path} 已存在。")
     
     def get_metric(self):
         """
@@ -46,9 +65,16 @@ class DecodeMetric(BaseMetric):
         # sentences = []
         # for ids in decode_list:
         #     sentences.append(self.tokenizer.decode(ids))
-        if (env.dp_rank == 0 or self.gather_result) and env.pp_rank == 0 and env.tp_rank == 0:
+        print(f"Debug Info: dp_rank={env.dp_rank}, pp_rank={env.pp_rank}, tp_rank={env.tp_rank}")
+        if env.dp_rank == 0 and env.pp_rank == 0 and env.tp_rank == 0:
+            print(f"Writing to file from process with dp_rank: {env.dp_rank}, pp_rank: {env.pp_rank}, tp_rank: {env.tp_rank}")
             if self.verbose:
                 logger.info(result["pred"])
             if self.save_to_file:
+                if "target" in result:
+                    to_write = [{"pred": pred, "target": target} for pred, target in zip(result["pred"], result["target"])]
+                else:
+                    to_write = [{"pred": pred} for pred in result["pred"]]
                 with open(self.save_path, 'a+') as f:
-                    f.write('\n'.join(result["pred"]) + '\n')
+                    for item in to_write:
+                        f.write(json.dumps(item, ensure_ascii=False) + '\n')
